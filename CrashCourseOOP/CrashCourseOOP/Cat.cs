af67853a6595482d269b6c61c24aacf7dbf768e8
98sergey4 <40461734+98sergey4@users.noreply.github.com>
2019-11-11 21:56:09 +0200

    Add files via upload

---
+++ CrashCourseOOP/CrashCourseOOP/Cat.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrashCourseOOP
+{
+    interface IPlayable
+    {
+        void play();
+        
+    }
+    internal abstract class Cat
+    {
+        private string name;
+        private string breed;
+
+        public string Name 
+        {
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                name = value;
+            }
+        }
+        public string Breed 
+        {
+            get
+            {
+                return breed;
+            }
+
+            set
+            {
+                breed = value;
+            }
+        }
+    }
+
+    internal class HomeCat : Cat, IPlayable
+    {
+        private string master;
+
+        public string Master 
+        {
+            get
+            {
+                return master;
+            }
+
+            set
+            {
+                master = value;
+            }
+        }
+
+        public HomeCat(string name, string breed, string mstr)
+        {
+            Name = name;
+            Breed = breed;
+            Master = mstr;
+        }
+
+        public void layOnSofa()
+        {
+            Console.WriteLine("Mhr-mhr-mhr");
+        }
+        public void spoilShoes()
+        {
+            Console.WriteLine("Spoiled!");
+        }
+
+        public void play()
+        {
+            Console.WriteLine("Playing with thread ball...");
+        }
+    }
+
+    internal class WildCat : Cat, IPlayable
+    {
+        public WildCat(string name, string breed)
+        {
+            Name = name;
+            Breed = breed;
+        }
+        public void hatePeople()
+        {
+            Console.WriteLine("Mew-mew, madafaka!");
+        }
+        public void play()
+        {
+            Console.WriteLine("Hunting for man...");
+        }
+    }
+}
---
+++ CrashCourseOOP/CrashCourseOOP/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrashCourseOOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HomeCat homeKitty1 = new HomeCat("Loki","Birman","Viktor");
+            HomeCat homeKitty2 = new HomeCat("Charlie", "Bengal", "Vasiliy");
+            WildCat wildKitty1 = new WildCat("Shadow","Bombay");
+            WildCat wildKitty2 = new WildCat("Lola", "Savannah");
+
+            List<Cat> cats = new List<Cat>() {homeKitty1,homeKitty2,wildKitty1,wildKitty2 };
+
+            foreach (Cat c in cats)
+            {
+                Console.WriteLine(c.Name + " " + c.Breed);
+            }
+        }
+    }
+}
